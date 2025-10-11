47f54fc2d298961cd441abd510eca65564eef663
Tenório Leal <tenorioleal09@gmail.com>
2025-10-11 18:22:02 +0000

    remova esse negocio (_for element <LinkComponent>_)

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -4,9 +4,7 @@ export function Header() {
   return (
     <header className="py-6 px-4 sm:px-6 lg:px-8 absolute top-0 left-0 w-full z-10">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-3xl font-headline font-bold text-foreground">
-          Nutri Morning
-        </Link>
+        
       </div>
     </header>
   );
